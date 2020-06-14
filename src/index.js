33759081320c3b5574b7c2d0096818a4fb54905c
Paul Varner <fuzzyray@gmail.com>
2020-06-14 12:24:10 -0500

    Fully working App

--- src/index.js
+++ src/index.js
@@ -3,50 +3,93 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const URL = "https://raw.githubusercontent.com/fuzzyray/FreeCodeCamp/master/assets/inspirational-quotes.json"
+const defaultQuote = [
+    {
+        quote: "To err is human, to really foul things up requires a computer",
+        author: "Unknown"
+    }
+];
 
-class Quote extends React.Component {
+class TweetThis extends React.Component {
+    /*
+        constructor(props) {
+            super(props);
+        }
+    */
+    render() {
+        if (this.props.current) {
+            return (
+                <p>
+                    <a id="tweet-quote"
+                       href={
+                           "https://twitter.com/intent/tweet?text="
+                           + encodeURIComponent(this.props.current.quote + " - " + this.props.current.author)}
+                       target="_blank"
+                       rel="noopener noreferrer">
+                        Tweet Quote
+                    </a>
+                </p>
+            );
+        } else {
+            return (
+                <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer">
+                    Tweet Quote
+                </a>
+            );
+        }
+    }
+}
+
+class QuoteBox extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            quotes: null
+            quotes: null,
+            current: null
         }
     }
 
     componentDidMount() {
-        fetch(URL).then(response => response.json()).then(json => this.setState({quotes: json}));
+        fetch(URL)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({quotes: json});
+                this.setState({current: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]});
+            })
+            .catch(err => {
+                console.error(err);
+                this.setState({quotes: defaultQuote, current: defaultQuote[0]});
+            });
     }
 
-    renderQuote(quotes) {
-        const quote = quotes[Math.floor(Math.random() * quotes.length)];
-        const text = quote.quote;
-        const author = quote.author;
+    renderQuote(current) {
         return (
             <React.Fragment>
-                <p id="text">{text}</p>
-                <p id="author">{author}</p>
+                <p id="text">{current.quote}</p>
+                <p id="author">{current.author}</p>
             </React.Fragment>
         )
     }
 
+    setQuote(quotes) {
+        if (quotes) {
+            this.setState({current: quotes[Math.floor(Math.random() * quotes.length)]});
+        }
+    }
+
     render() {
         const quotes = this.state.quotes;
+        const current = this.state.current;
 
         return (
-            <div>
-                { (quotes) ? this.renderQuote(quotes) : "Fetching quotes...." }
-                <button id="new-quote">New Quote</button>
+            <div id="quote-box">
+                {(quotes && current) ? this.renderQuote(current) : <p>Fetching quotes....</p>}
+                <button id="new-quote" onClick={() => this.setQuote(quotes)}>New Quote</button>
+                <TweetThis current={this.state.current}/>
+                { /* <a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet Quote</a> */}
             </div>
         );
     }
-}
-
-function QuoteBox() {
-    return (
-        <div id="quote-box">
-            <Quote/>
-            <a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet Quote</a>
-        </div>
-    );
 }
 
 function App() {
